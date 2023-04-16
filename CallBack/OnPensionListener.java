10d74e649ab7bb781847d5d2a431330c6ddde4fb
Gevorg Kocharyan <g.b.kocharyan@gmail.com>
2023-04-16 14:48:35 +0400

    deleted some classes

--- CallBack/OnPensionListener.java
+++
@@ -1,7 +0,0 @@
-package com.company.CallBack;
-
-public interface OnPensionListener {
-
-    void onPensionReceived(double pensionAmount);
-
-}
--- News/OnNewPostListener.java
+++
@@ -1,5 +0,0 @@
-package com.company.News;
-
-public interface OnNewPostListener {
-    void newPostPublished(String content);
-}
